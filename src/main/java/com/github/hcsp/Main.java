dee4d571bb5fa03188ae6a26598d2d46a48313ba
Y.L <2180531663@qq.com>
2022-03-06 21:48:54 +0800

    创建类并输出hello

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -2,9 +2,10 @@ package com.github.hcsp;
 
 public class Main {
     // Create a public static void main(String[] args) method here
-    // and print "Hello" to standard output (System.out)
+    // and print "Hello" to standa
+    // rd output (System.out)
     // 在这里创建一个public static void main(String[] args)方法，在标准输出（System.out）打印"Hello"
     public static void main(String[] args) {
-        System.out.printf("Hello");
+        System.out.println("Hello");
     }
 }
